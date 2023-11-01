66f9e464462e6181647f24d4dca630a62b8787f0
AortizVale <aortiz449@unab.edu.co>
2023-11-01 09:01:08 -0500

    Actualizacion Spatie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,18 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-
             $table->string('tipo_doc',50);
             $table->string('num_doc',50);
             $table->string('nombre1',50);
             $table->string('nombre2',50);
             $table->string('apellido1',50);
             $table->string('apellido2',50);
-            
             $table->rememberToken();
             $table->timestamps();
         });
