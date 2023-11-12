b94e332122fdf93c4f0c3f64debbe994b11fb979
AortizVale <aortiz449@unab.edu.co>
2023-11-12 18:40:20 -0500

    Middleware noCache

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,26 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ConsultorioController;
 use App\Http\Controllers\UsersController;
 
+Route::middleware('noCache')->group(function () {
+    Route::get('/', [AuthController::class, "homePage"])->name('home');
 
+    Route::get('ingreso', [PacientesController::class, "listPacientes"])->name('ingreso');
 
-Route::get('/', [AuthController::class, "homePage"])->name('home');
+    Route::get('gestor', [PacientesController::class, "vistaMedico"])->name('gestor');
 
-Route::get('ingreso', [PacientesController::class, "listPacientes"])->name('ingreso');
+    Route::get('llamado', [PacientesController::class, "vistaLlamado"])->name('llamado')->middleware('auth', "can:llamada");
 
-Route::get('gestor', [PacientesController::class, "vistaMedico"])->name('gestor');
+    Route::get('programacion', [PacientesController::class, "vistaProgramacion"])->name('programacion');
 
-Route::get('llamado', [PacientesController::class, "vistaLlamado"])->name('llamado')->middleware('auth',"can:llamada",'noCache');
+    Route::get('confirmacion', [PacientesController::class, "vistaConfirmacion"])->name('confirmacion');
 
-Route::get('programacion', [PacientesController::class, "vistaProgramacion"])->name('programacion');
+    Route::get('login', [AuthController::class, "loginPage"])->name('login');
 
-Route::get('confirmacion', [PacientesController::class, "vistaConfirmacion"])->name('confirmacion');
+    Route::get('registro', [AuthController::class, "registerPage"])->name('registro');
+    Route::post('register', [UsersController::class, "store"])->name('registro.store');
+    Route::post('iniciar_s', [UsersController::class, "login"])->name('login.auth');
+    Route::post('logout', [UsersController::class, 'logout'])->name('logout');
 
-Route::get('login', [AuthController::class, "loginPage"])->name('login');
-
-Route::get('registro', [AuthController::class, "registerPage"])->name('registro'); 
-Route::post('register', [UsersController::class, "store"])->name('registro.store'); 
-Route::post('iniciar_s', [UsersController::class, "login"])->name('login.auth'); 
-Route::post('logout', [UsersController::class, 'logout'])->name('logout');
-
-Route::get('consultorio/create', [ConsultorioController::class, "create"])->name('consultorio.create'); 
-Route::post('consultorio', [ConsultorioController::class, "store"])->name('consultorio.store'); 
-
-
-
+    Route::get('consultorio/create', [ConsultorioController::class, "create"])->name('consultorio.create');
+    Route::post('consultorio', [ConsultorioController::class, "store"])->name('consultorio.store');
+});
